f69d0e3e2bee6c2b60a99892437b0eace2115cbd
Sam Bossley <bossley.samuel@gmail.com>
2023-04-30 18:21:08 -0700

    feat: highlight current section

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,34 @@
 import { SITE_TITLE } from '@/constants'
 
-export function Header() {
+type Props = { currentUrl: URL }
+
+export function Header({ currentUrl }: Props) {
+  const getClasses = (path: string): string | undefined => {
+    return currentUrl.pathname.startsWith(path) ? 'active' : undefined
+  }
+
   return (
     <header className="donotprint">
       <nav>
         <a id="title" href="/">
           {SITE_TITLE}
         </a>
-        <a href="/thoughts">thoughts</a>
+        <a className={getClasses('/thoughts')} href="/thoughts">
+          thoughts
+        </a>
         <a href="https://git.sr.ht/~bossley9">repos</a>
-        <a href="/tabs">tabs</a>
-        <a href="/poems">poems</a>
-        <a href="/recipes">recipes</a>
-        <a href="/recs">recs</a>
+        <a className={getClasses('/tabs')} href="/tabs">
+          tabs
+        </a>
+        <a className={getClasses('/poems')} href="/poems">
+          poems
+        </a>
+        <a className={getClasses('/recipes')} href="/recipes">
+          recipes
+        </a>
+        <a className={getClasses('/recs')} href="/recs">
+          recs
+        </a>
       </nav>
     </header>
   )
