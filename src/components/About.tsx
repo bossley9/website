c33bb482cd3886a2dca1864eb09d1e752d076887
Sam Bossley <bossley.samuel@gmail.com>
2023-05-04 21:57:46 -0700

    feat: add photo of me

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,21 +4,27 @@ export function About() {
   return (
     <section className="about">
       <h1>Hello, I&#39;m Sam (he/him/his)!</h1>
-      <p>
-        I&#39;m a 20-something year old software engineer living in Columbus.
-      </p>
-      <p>
-        In my freetime I enjoy writing, reading, music, memes, creating art, and
-        playing video games.
-      </p>
-      <p>
-        I&#39;m a minimalist by nature and love everything tech, security, and
-        libre.
-      </p>
-      <p>
-        <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
-        <a rel="me" href="https://mas.to/@bossley9"></a>
-      </p>
+      <div className="stacked">
+        <img className="me" src="/static/me.jpg" alt="a portrait of me" />
+        <div>
+          <p>
+            I&#39;m a 20-something year old frontend software engineer living in
+            Columbus.
+          </p>
+          <p>
+            In my freetime I enjoy writing, reading, music, memes, creating art,
+            programming, and playing video games.
+          </p>
+          <p>
+            I&#39;m a minimalist by nature and love everything tech, security,
+            and libre.
+          </p>
+          <p>
+            <a href={`mailto:${EMAIL}`}>{EMAIL}</a>
+            <a rel="me" href="https://mas.to/@bossley9"></a>
+          </p>
+        </div>
+      </div>
     </section>
   )
 }
