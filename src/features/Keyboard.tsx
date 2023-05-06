4b3a17ed11a7edafc083c13c34ca304ddf1958b4
Sam Bossley <bossley.samuel@gmail.com>
2023-05-05 18:04:17 -0700

    fix: unify spacing between code links on keyboard page

--- src/features/Keyboard.tsx
+++ src/features/Keyboard.tsx
@@ -3,16 +3,20 @@ export function Keyboard() {
     <section className="keyboard">
       <h1>Keyboard</h1>
       <p>
-        My current daily driver is a{' '}
+        My current daily driver is a&nbsp;
         <code>
           <a href="https://www.zsa.io/moonlander/">
             moonlander (detached wings)
-          </a>{' '}
-          with{' '}
+          </a>
+        </code>
+        &nbsp;with&nbsp;
+        <code>
           <a href="https://www.cherrymx.de/en/cherry-mx/mx-original/mx-silent-red.html">
             cherry red silents
-          </a>{' '}
-          and{' '}
+          </a>
+        </code>
+        &nbsp;and&nbsp;
+        <code>
           <a href="https://esckeyboard.com/products/pom-sugarcube-keycaps-1">
             jelly pom keycaps
           </a>
