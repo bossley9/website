4874be0ff48809e3a7a5dd82dbc1a96eac13b555
Sam Bossley <sam@bossley.xyz>
2024-01-14 10:50:50 -0500

    feat(packages): remove zod

--- deps.ts
+++ deps.ts
@@ -1,5 +1,3 @@
-export { z, ZodError } from "https://deno.land/x/zod@v3.22.4/index.ts";
-export { fromZodError } from "npm:zod-validation-error@2.1.0";
 export { markdownIt } from "lume/deps/markdown_it.ts";
 export { minify } from "lume/deps/minify_html.ts";
 export { defaults as minifyDefaultOptions } from "lume/plugins/minify_html.ts";
